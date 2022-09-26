10e57cf144e8582335289eed52126d12a85fddd6
Chinmaya Kumar Biswal <chinmayakumarbiswal16045@gmail.com>
2022-09-26 14:20:00 +0530

    Adding logout page

---
+++ login/include/logout.php
@@ -0,0 +1,12 @@
+<?php
+    session_start();
+    if(isset($_SESSION['email']))
+    {
+        session_destroy();
+        header('location:../auth/login.php');
+    }
+    else
+    {
+        header('location:../auth/login.php');
+    }
+?>
