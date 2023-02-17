b8e95d39efd406dd38f7128b1a0d2f047854330a
Chinmaya Kumar Biswal <chinmayakumarbiswal16045@gmail.com>
2023-02-17 20:30:04 +0530

    make profile public or private notification

--- login/teacher/makeprofile.php
+++ login/teacher/makeprofile.php
@@ -3,11 +3,8 @@
     $email=$_GET['email'];
     $profile=$_GET['profile'];
 
-    echo $email;
-    echo $profile;
-
     $query="UPDATE `teacher` SET `public`='$profile' WHERE email='$email'";
     $testset=mysqli_query($db,$query);
-    header('location:./teacher.php');
+    echo "<script>alert('Your profile now ".$profile." .');window.location.href = './teacher.php';</script>";
     
 ?>
