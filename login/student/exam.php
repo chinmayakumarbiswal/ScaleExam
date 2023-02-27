7a736e12641b42f9fd5137fa4ce71525884ad940
Chinmaya Kumar Biswal <chinmayakumarbiswal16045@gmail.com>
2023-02-27 22:05:25 +0530

    change footer of student

--- login/student/exam.php
+++ login/student/exam.php
@@ -262,12 +262,9 @@ $currentTimeIs=date('H:i', time());
           </div>
           <!-- content-wrapper ends -->
           <!-- partial:../partials/_footer.html -->
-          <footer class="footer">
-            <div class="d-sm-flex justify-content-center justify-content-sm-between">
-              <span class="text-muted d-block text-center text-sm-left d-sm-inline-block">Copyright © scaleexam.in </span>
-              <span class="float-none float-sm-right d-block mt-1 mt-sm-0 text-center"> This is made with ❤️ by <a href="https://www.bootstrapdash.com/bootstrap-admin-template/" target="_blank">Chinmaya </a></span>
-            </div>
-          </footer>
+          <?php
+            include_once('../include/footer.php')
+          ?>
           <!-- partial -->
         </div>
         <!-- main-panel ends -->
